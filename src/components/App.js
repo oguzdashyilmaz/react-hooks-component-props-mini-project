7a4a85500ac0ab5518fc2f7e29a7ccb584744ea3
Oguz <yilmazoguz@outlook.com>
2022-02-03 21:54:56 +0300

    Create and add About component to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import blogData from "../data/blog";
 import Header from "../components/Header";
+import About from "../components/About"
 
 // console.log(blogData);
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header name={blogData.name}/>
+      <About/>
     </div>
   );
 }
