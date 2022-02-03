b0d6885c55ea1ee2b5ba30d394ac804f9736e110
Oguz <yilmazoguz@outlook.com>
2022-02-03 21:52:10 +0300

    Add name props to Header in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import blogData from "../data/blog";
 import Header from "../components/Header";
 
-console.log(blogData);
+// console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
-      <Header/>
+      <Header name={blogData.name}/>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-function Header() {
-  return <div></div>;
+function Header({name}) {
+  return (
+    <header>
+        <h1>{name}</h1>
+    </header>
+  );
 }
 
 export default Header;
